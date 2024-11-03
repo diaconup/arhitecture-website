421259a2350b72f255ffc3f0439c412e2c83ebd5
Diaconu Paul <152711917+diaconup@users.noreply.github.com>
2024-11-03 14:41:31 +0200

    Multi language fix

--- src/screens/portofolio/index.tsx
+++ src/screens/portofolio/index.tsx
@@ -1,12 +1,16 @@
 import React from 'react';
-import items from '../../utils/items.json';
+import { useTranslation } from 'react-i18next';
+import items_ro from '../../utils/items_ro.json';
+import items_en from '../../utils/items_en.json';
 import { Item } from '../../utils/types';
 import ListItem from '../../components/ListItem/index';
 import { styles } from './styles';
 import BackToTop from '../../components/BackToTop';
 
-
 const Portfolio: React.FC = () => {
+  const { i18n } = useTranslation(); 
+  const items = i18n.language === 'ro' ? items_ro : items_en; 
+
   return (
     <div style={styles.portfolioContainer}>
       {items.map((item: Item, index: number) => (
